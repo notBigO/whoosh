57dc1b3c6210013e2f4434b0b491b0fdb0d73c12
notBigO <2003varunp@gmail.com>
2025-07-09 17:36:06 +0530

    Update Electron configuration and package dependencies; add new Radix UI components and refactor App component for improved styling and structure.

--- client/src/renderer/src/App.tsx
+++ client/src/renderer/src/App.tsx
@@ -1,11 +1,5 @@
 const App = () => {
-  return (
-    <div className="h-screen bg-zinc-900 text-gray-100 flex items-center justify-center text-center font-sans">
-      <div className="space-y-4">
-        <h1 className="text-2xl font-medium text-white">Whoosh Tray App is Working!</h1>
-      </div>
-    </div>
-  )
+  return <div>App</div>
 }
 
 export default App
